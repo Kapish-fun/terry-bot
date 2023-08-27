bd5d5b3ef82c848457bed101253150abdab98b33
cirroskais <34550332+I-Have-An-Issue@users.noreply.github.com>
2023-08-27 19:33:05 -0400

    Add render command

---
+++ src/commands/render.js
@@ -0,0 +1,45 @@
+const { format } = require("util")
+
+const endpoints = {
+	headshot: "render/user/%d/headshot",
+	bodyshot: "render/user/%d/bodyshot",
+	asset: "render/asset/%d",
+	clothing: "render/clothing/%d",
+}
+
+exports.info = {
+	name: "render",
+	description: "Render an asset.",
+	disabled: false,
+	options: [
+		{
+			name: "type",
+			description: "The render type.",
+			type: 3,
+			required: true,
+			choices: [
+				{ name: "Headshot", value: "headshot" },
+				{ name: "Bodyshot", value: "bodyshot" },
+				{ name: "Asset", value: "asset" },
+				{ name: "Clothing", value: "clothing" },
+			],
+		},
+		{ name: "id", description: "The asset ID.", type: 4, required: true },
+	],
+}
+
+exports.run = async (client, interaction) => {
+	const { data, member, guildID } = interaction
+	const type = data.options[0].value
+	const id = data.options[1].value
+
+	await interaction.defer()
+
+	const response = await fetch(`http://${process.env.ARBITER_URL}/${format(endpoints[type], id)}?key=${process.env.ARBITER_KEY}`).catch((_) => _)
+	if (response?.message) return interaction.createMessage("The arbiter is currently down.")
+
+	const file = await response.text()
+	if (!file) return interaction.createMessage("The arbiter did not return any data. The site might be down.")
+
+	return interaction.createMessage({ embeds: [] }, { file: Buffer.from(file, "base64"), name: "render.png" })
+}
