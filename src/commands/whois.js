c9c1f0be4f1999b7b09a269de66ba38246708d64
cirroskais <34550332+I-Have-An-Issue@users.noreply.github.com>
2023-09-05 17:05:04 -0400

    :3

--- src/commands/whois.js
+++ src/commands/whois.js
@@ -3,12 +3,17 @@ const endpoint = "https://kapish.fun/discord/match?discordId="
 exports.info = {
 	name: "whois",
 	description: "Lookup a Kapish user by their Discord account.",
-	disabled: true,
+	disabled: false,
 	options: [{ name: "user", description: "The Discord user to lookup.", type: 9, required: true }],
 }
 
 exports.run = async (client, interaction) => {
 	const { id, token, data, channel, member } = interaction
+	const user = data.options[0].value
 
-	return interaction.createMessage(`this shit is NOT ready yet`)
+	const response = await fetch(endpoint + user)
+	const { success, userId, username } = await response.json()
+
+	if (!success) return interaction.createMessage(`That user does not have a Kapish account linked.`)
+	return interaction.createMessage(`[${username}](https://kapish.fun/users/${userId}/profile)`)
 }
