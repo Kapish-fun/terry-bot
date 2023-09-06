e035b95555228b4d063d84f2ac46b4cb08143167
cirroskais <143854928+cirroskais@users.noreply.github.com>
2023-09-06 01:34:52 -0400

    Update messageReactionAdd.js

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -2,7 +2,7 @@ module.exports = async (client, message, emoji, reactor) => {
 	if (emoji.name !== "🍅") return
 	if (!message.reactions) message = await client.getMessage(message.channel.id, message.id)
 	if (message.author.id == client.user.id) return
-	if (message.guild.id == "1130286476031184906") return //being abused 2 much
+	if (message.guildID == "1130286476031184906") return //being abused 2 much
 	if (["632337309785915412", "1079988920776929361", "249696282711556107"].includes(message.author.id)) return
 	if (!["1130564006449528842", "1142544785157144717", "1142544337360654406"].includes(message.channel.id)) return
 
@@ -17,7 +17,7 @@ module.exports = async (client, message, emoji, reactor) => {
 		})
 		client.notifiedMessages.add(message.id)
 	} else if (coalReactions >= 10) {
-		message.member.edit({ communicationDisabledUntil: new Date(Date.now() + 1000 * 60 * 60 * 30) }).catch((_) => {
+		message.member.edit({ communicationDisabledUntil: new Date(Date.now() + 1000 * 60 * 30) }).catch((_) => {
 			console.log(_)
 		})
 		message.delete().catch((_) => {})
